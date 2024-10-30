4a5a2b4574009319c6debf58d48c720264679ed9
Harsha Mohan <harshammg@gmail.com>
2024-10-30 19:55:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const splashScreen = document.getElementById('splash-screen');
+  setTimeout(() => {
+    splashScreen.style.display = 'none';
+  }, 2000); // Splash screen duration (in milliseconds)
+});
